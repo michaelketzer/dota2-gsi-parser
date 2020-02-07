35f3adafff559260c0350e506a771420400c3e67
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-02-07 21:59:31 +0100

    Added client heartbeat;

--- src/Transport.ts
+++ src/Transport.ts
@@ -3,20 +3,39 @@ import WSClient from 'ws';
 export default class Transport {
     ws: WSClient;
     wsURI: string;
+    pingTimeout: NodeJS.Timeout | null = null;
     
     constructor(wsURI: string) {
         this.wsURI = wsURI;
         this.ws = new WSClient(this.wsURI);
-	this.ws.on('close', () => this.reconnect());
+        this.ws.on('open', this.heartbeat);
+        this.ws.on('ping', this.heartbeat);
+	    this.ws.on('close', () => this.reconnect());
+    }
+
+    heartbeat(): void {
+        this.pingTimeout && clearTimeout(this.pingTimeout);
+    
+        // Use `WebSocket#terminate()`, which immediately destroys the connection,
+        // instead of `WebSocket#close()`, which waits for the close timer.
+        // Delay should be equal to the interval at which your server
+        // sends out pings plus a conservative assumption of the latency.
+        this.pingTimeout = setTimeout(() => {
+            this.ws.terminate();
+            console.log('Heartbeat failed. Terminated connection.');
+        }, 30000 + 1000);
     }
 
     resetWS(): void {
         this.ws = new WSClient(this.wsURI);
-	this.ws.on('close', () => this.reconnect());
+        this.ws.on('open', this.heartbeat);
+        this.ws.on('ping', this.heartbeat);
+	    this.ws.on('close', () => this.reconnect());
     }
 
     reconnect(): void {
-        setTimeout(() => this.resetWS(), 2500);
+        this.pingTimeout && clearTimeout(this.pingTimeout);
+        setTimeout(() => this.resetWS(), 500);
     }
 
     isReady(): boolean {
